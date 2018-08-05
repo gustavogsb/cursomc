94509af7563d610c25987afc45a398bb24f06849
Gustavo Bastos <gustavogsb@gmail.com>
2018-08-05 15:41:08 -0300

    Rest Testado

--- src/main/java/com/gustavobastos/cursomc/CursomcApplication.java
+++ src/main/java/com/gustavobastos/cursomc/CursomcApplication.java
@@ -2,7 +2,7 @@ package com.gustavobastos.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//https://git-scm.com/book/pt-br/v1/Primeiros-passos-Configura%C3%A7%C3%A3o-Inicial-do-Git
 @SpringBootApplication
 public class CursomcApplication {
 
