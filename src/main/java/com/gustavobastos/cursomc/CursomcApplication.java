a7a2afce7acbe23a7089effd8b895ebd78e9f262
Gustavo Bastos <gustavogsb@gmail.com>
2018-08-08 15:07:57 -0300

    Criando operação de instanciacao

--- src/main/java/com/gustavobastos/cursomc/CursomcApplication.java
+++ src/main/java/com/gustavobastos/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.gustavobastos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.gustavobastos.cursomc.domain.Categoria;
+import com.gustavobastos.cursomc.repositories.CategoriaRepository;
 //https://git-scm.com/book/pt-br/v1/Primeiros-passos-Configura%C3%A7%C3%A3o-Inicial-do-Git
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+	
+	
 }
