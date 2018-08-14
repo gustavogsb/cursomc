dff882080f3cba670ad39da690eff68aa6c6cb99
Gustavo Bastos <gustavogsb@gmail.com>
2018-08-13 23:12:40 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/gustavobastos/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,17 @@
+package com.gustavobastos.cursomc.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gustavobastos.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	Optional<Produto> findById(Integer id);
+	
+	
+
+}
